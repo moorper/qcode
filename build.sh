8353604bb3638d4560653940adb3c61b3218c79d
liuzhaowei <liuzhaowei55@gmail.com>
2018-01-26 15:08:34 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm -rf /Users/where/tmp/qcode
-rm -rf /Users/where/tmp/qcode.zip
+rm -rf ~/tmp/qcode
+rm -rf ~/tmp/qcode.zip
 
-mkdir /Users/where/tmp/qcode
-rsync -rz ./css /Users/where/tmp/qcode
-rsync -rz ./img /Users/where/tmp/qcode
-rsync -rz ./js /Users/where/tmp/qcode
-rsync ./manifest.json /Users/where/tmp/qcode
-rsync ./option.html /Users/where/tmp/qcode
-rsync ./popup.html /Users/where/tmp/qcode
-cd /Users/where/tmp
+mkdir -p ~/tmp/qcode
+rsync -rz ./css ~/tmp/qcode
+rsync -rz ./img ~/tmp/qcode
+rsync -rz ./js ~/tmp/qcode
+rsync ./manifest.json ~/tmp/qcode
+rsync ./option.html ~/tmp/qcode
+rsync ./popup.html ~/tmp/qcode
+cd ~/tmp
 zip -rq qcode.zip qcode
-rm -rf /Users/where/tmp/qcode
+rm -rf ~/tmp/qcode
